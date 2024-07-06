587013803ccc0dc4bea369c216dd7aa744f22447
Any31415 <123270760+Any31415@users.noreply.github.com>
2024-07-06 12:44:48 +0500

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,5 +1,14 @@
-{pkgs}: {
+{ pkgs }: {
   deps = [
+    pkgs.nodejs
+    pkgs.nodePackages.npm
     pkgs.wget
-   ];
+  ];
+
+  shellHook = ''
+    mkdir -p ~/.postgresql
+    wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" --output-document ~/.postgresql/root.crt
+    chmod 0600 ~/.postgresql/root.crt
+    npm install pg
+  '';
 }
